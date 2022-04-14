5ce67f89ad43d59d3eb7446e2a0b55d5830f60b1
Ekramz <smahmud1098@gmail.com>
2022-04-14 13:18:30 +0600

    login and signup inpput filed  value collection organizd

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -5,18 +5,20 @@ import { Link } from 'react-router-dom';
 
 const Login = () => {
     const emailRef=useRef('')
+    const passwordRef=useRef('')
 
     const handleSubmit=e=>{
         e.preventDefault()
        
-        console.log(emailRef)
+        console.log(emailRef.current.value)
+        console.log(passwordRef.current.value)
     }
     
     return (
         <div className='w-50 mx-auto '>
 
             <h1 className='text-primary text-center'>Please Login</h1>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control ref={emailRef} type="email" placeholder="Enter email" />                  
@@ -24,18 +26,16 @@ const Login = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" />
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
+                
 
                 <Button variant="primary" type="submit">
                     Submit
                 </Button>
             </Form>
-            <Link className='text-decoration-none text-dark' to="/signup">New to Car genious? <span className='text-primary '>register</span></Link>
+            <Link className=' mt-3 text-decoration-none text-dark' to="/signup">New to Car genious? <span className='text-primary '>register</span></Link>
         </div>
     );
 };
--- src/Pages/Login/Signup/Signup.js
+++ src/Pages/Login/Signup/Signup.js
@@ -4,21 +4,25 @@ import { Link } from 'react-router-dom';
 
 
 const Signup = () => {
+    const nameRef=useRef('')
     const emailRef=useRef('')
+    const passwordRef=useRef('')
 
     const handleSubmit=e=>{
         e.preventDefault()
        
-        console.log(emailRef)
+        console.log(nameRef.current.value)
+        console.log(emailRef.current.value)
+        console.log(passwordRef.current.value)
     }
     return (
         <div className='w-50 mx-auto '>
 
         <h1 className='text-primary text-center'>Please Register</h1>
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicName">
                 <Form.Label>Name</Form.Label>
-                <Form.Control ref={emailRef} type="text" placeholder="Enter Name" />                  
+                <Form.Control ref={nameRef} type="text" placeholder="Enter Name" />                  
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -28,18 +32,15 @@ const Signup = () => {
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control ref={passwordRef} type="password" placeholder="Password" />
             </Form.Group>
-
-            {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group> */}
+           
 
             <Button variant="primary" type="submit">
                 Submit
             </Button>
         </Form>
-        <Link className='text-decoration-none text-dark' to="/login">Have an Account? <span className='text-primary '>Login</span></Link>
+        <Link className='mt-3 text-decoration-none text-dark' to="/login">Have an Account? <span className='text-primary '>Login</span></Link>
     </div>
     );
 };
